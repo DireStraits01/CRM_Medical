5fc7983be4a1c53938d37b4c693ad601808108d8
DireStraits01 <arsamak88@gmail.com>
2020-12-06 01:44:36 +0300

    Revert "static files"
    This reverts commit ba8b8317788403fb1d1e563a53a4fe00d78b7d9b.

--- crmn/settings.py
+++ crmn/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATICFILE_DIRS = [
-#   "static/images",
-#  "static/css",
-# "staticfiles", ]
+STATICFILE_DIRS = [
+    "static/images",
+    "static/css",
+    "staticfiles", ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
