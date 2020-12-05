8e721090d4e4c5cf03e9453714b1b5ba6b5f3792
DireStraits01 <arsamak88@gmail.com>
2020-12-06 02:18:23 +0300

    add whitenoise

--- crmn/settings.py
+++ crmn/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'crmn.urls'
@@ -121,12 +122,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 STATICFILE_DIRS = [
-    "static/images",
-    "static/css",
-    "staticfiles", ]
+    os.path.join(BASE_DIR, 'static'),
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+# STATICFILE_DIRS = [
+#   "static/images",
+#  "static/css",
+# "staticfiles", ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
