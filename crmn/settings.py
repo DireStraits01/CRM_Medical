ec828a64d41b890ed3e25a95ca149af2078d972f
DireStraits01 <arsamak88@gmail.com>
2021-01-25 07:47:25 +0300

    collect sttaic

--- crmn/settings.py
+++ crmn/settings.py
@@ -126,10 +126,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
+STATIC_URL = '/static/'
+if DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+else:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
 
 
 MEDIA_URL = '/media/'
